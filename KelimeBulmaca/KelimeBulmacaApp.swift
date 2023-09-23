8dcdd2c06ccc3963f7f3d3bac72ecb583a97aa2e
Alparslan Buğra Selvi <alparslan.bugra@hotmail.com>
2023-09-23 22:34:14 +0300

    Initial Commit

---
+++ KelimeBulmaca/KelimeBulmacaApp.swift
@@ -0,0 +1,17 @@
+//
+//  KelimeBulmacaApp.swift
+//  KelimeBulmaca
+//
+//  Created by Alp Selvi on 23.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct KelimeBulmacaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
